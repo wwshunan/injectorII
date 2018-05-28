254dbc9e3babeb04ad0da2f37a51026537512b97
wws <wws@localhost.localdomain>
2018-05-28 16:48:06 +0800

    complex side menu

---
+++ app/static/js/custom/breadcrumb.js
@@ -0,0 +1,11 @@
+$('.menu-list a').on('click', function() {
+  var $this = $(this),
+      $bc = $('<div class="item"></div>');
+
+  $this.parents('li').each(function(n, li) {
+      var $a = $(li).children('a').clone();
+      $bc.prepend(' / ', $a);
+  });
+    $('.breadcrumb').html( $bc.prepend('<a href="#home">Home</a>') );
+    return false;
+}) 
